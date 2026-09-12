0c9ad1a8c5ee886987b050ff766cf39fb8e62225
Musa Rasa <mosarasafaroqi@gmail.com>
2026-09-12 10:27:15 +0430

    Simple Calculator
    Simple calculate 4 main arithmetic operation, I did four main arithmitic operation (+,-,*,/) .

---
+++ aritmetic.java
@@ -0,0 +1,15 @@
+public class aritmetic {
+    public static void main(String[] args){
+        int  a = 12;
+        int b = 12;
+        int sum = a+b;
+        System.out.println("sum = "+sum);
+        int sub = a-b;
+        System.out.println("sum = "+sub);
+        int mult = a*b;
+        System.out.println("sum = "+mult);
+        int dev = a/b;
+        System.out.println("sum = "+dev);
+    }
+    
+}
